20d86c734f929cd08efdde0a321cd2548ca7d9e5
nevmstas <nevmyvakastas@gmail.com>
2023-01-12 21:46:18 +0600

    shorten address func & version update

--- client/src/Components/atoms/card-wallet-address/WalletAddress.tsx
+++ client/src/Components/atoms/card-wallet-address/WalletAddress.tsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react'
+import { TransactionContext } from '../../../context/TransactionContext'
+import { shortenAddress } from '../../../utils/shortenAddress'
+
 const WalletAddress = () => {
+    const { currentAccount } = useContext(TransactionContext)
     return (
         <div>
-            <p className="text-white font-light text-sm">Address</p>
+            <p className="text-white font-light text-sm">
+                {shortenAddress(currentAccount)}
+            </p>
             <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
         </div>
     )
